8fc10616a49d646c538cc8c85da50c4cc2362320
Joe <hutcheson2001@yahoo.com>
2022-03-24 15:14:28 -0400

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { actors } from "../data";
 
+function Movies({actor}){
+  const movieList = actor.movies.map(movie => {
+    return (
+      <li key = {movie}>{movie}</li>
+    )
+    })
+  return movieList
+}
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorList = actors.map(actor=>{
+    return (
+      <div key = {actor.name}>
+        <header>{actor.name}</header>
+        <ul><Movies actor={actor}/></ul>
+      </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actorList}
+  </div>);
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "black",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+      to="/"
+      exact
+   
+      >
+        Home
+      </NavLink>
+      <NavLink 
+      to="/movies"
+     
+      >
+        Movies
+      </NavLink>
+      <NavLink 
+      to="/directors"
+      exact
+     
+      >
+        Directors
+      </NavLink>
+      <NavLink 
+      to="/actors"
+      exact
+      >
+        Actors
+      </NavLink>
+  </div>
+  )
+  
 }
 
 export default NavBar;
